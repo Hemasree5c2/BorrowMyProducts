973824b93439d558f04f53ee246b36b9d494b466
Nelluru-Hemasree <hemasree.nelluru@zemosolabs.com>
2020-04-17 17:44:29 +0530

    Project Structure

---
+++ BorrowMyProducts/src/main/java/com/hackathon/borrowmyproducts/BorrowMyProductsApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.borrowmyproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowMyProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowMyProductsApplication.class, args);
+	}
+
+}
