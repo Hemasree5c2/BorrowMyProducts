74da70f169523ca803ae0dfa0852b48f8cb7d3f2
Nelluru-Hemasree <hemasree.nelluru@zemosolabs.com>
2020-04-22 12:47:26 +0530

    updated signup page

--- BorrowMyProducts/src/main/java/com/techhunters/borrowmyproducts/controller/AuthController.java
+++ BorrowMyProducts/src/main/java/com/techhunters/borrowmyproducts/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Controller
 public class AuthController {
@@ -17,10 +18,13 @@ public class AuthController {
     public String register() {
         return "signup";
     }
-    @PostMapping("/validate")
+    @PostMapping("/")
     public String validate(@ModelAttribute(name="user") UserDTO user, Model model) {
+        if(user.getLatitude()=="") {
+            model.addAttribute("locationError",true);
+            return "login";
+        }
         if(objects.size()!=0) {
-            System.out.println(objects.size());
             for (int i = 0; i < objects.size(); i++) {
                 if (objects.get(i).getEmail().equals(user.getEmail()) && objects.get(i).getPassword().equals(user.getPassword())) {
                     model.addAttribute("user", objects.get(i));
@@ -31,8 +35,28 @@ public class AuthController {
         model.addAttribute("InvalidCredentials",true);
         return "login";
     }
-    @PostMapping("/append")
+    @PostMapping("/signup")
     public String append(@ModelAttribute(name="user") UserDTO user,Model model) {
+        String regexEmail="^(.+)@(.+)$";
+        Pattern pattern = Pattern.compile(regexEmail);
+        if(!pattern.matcher(user.getEmail()).matches()) {
+            model.addAttribute("emailError",true);
+            return "signup";
+        }
+        if(!user.getPassword().equals(user.getConfirmPassword())) {
+            model.addAttribute("passwordError",true);
+            return "signup";
+        }
+        String regexPhone="[0-9]+";
+        Pattern pat=Pattern.compile(regexPhone);
+        if(!pat.matcher(user.getPhoneNo()).matches()) {
+            model.addAttribute("numberError",true);
+            return "signup";
+        }
+        if(!pat.matcher(user.getPincode()).matches()) {
+            model.addAttribute("numberError",true);
+            return "signup";
+        }
         objects.add(user);
         return "login";
     }
